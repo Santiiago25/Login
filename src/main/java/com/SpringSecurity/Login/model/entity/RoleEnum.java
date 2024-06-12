f2d0ab1695763b01a90bfca9982db4f1b863b888
Santiago Tokora <kevintocora_3d@hotmail.com>
2024-06-12 12:47:15 -0500

    Spring Security

---
+++ src/main/java/com/SpringSecurity/Login/model/entity/RoleEnum.java
@@ -0,0 +1,8 @@
+package com.SpringSecurity.Login.model.entity;
+
+public enum RoleEnum {
+    ADMIN,
+    USER,
+    INVITED,
+    DEVELOPER
+}
---
+++ src/main/java/com/SpringSecurity/Login/model/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.SpringSecurity.Login.model.repository;
+
+import com.SpringSecurity.Login.model.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    Optional<UserEntity> findUserEntityByUsername(String username);
+}
